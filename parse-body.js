307a0df681532583042a0386b0f2cfafef841e83
techinvestorsza <techinvestorsza@gmail.com>
2023-12-27 19:50:19 +0200

    completed project

--- parse-body.js
+++ parse-body.js
@@ -1,25 +1,57 @@
 function firstStep(input) {
   // Your code here
+  return input.split('&');
 }
 
 function secondStep(input) {
   // Your code here
+  return input.map(ele => {
+    return ele.split('=');
+  });
 }
 
 function thirdStep(input) {
-  // Your code here
+  return input.map(ele => {
+    return ele.map(i => {
+    return i.replace('+', ' ')
+    });
+})
 }
 
 function fourthStep(input) {
   // Your code here
+  return input.map(ele => {
+    return ele.map(i => {
+      return decodeURIComponent(i);
+    })
+  })
 }
 
 function fifthStep(input) {
   // Your code here
+      /*
+      // empty obj
+  // loop through - first item is the key, second item is the value
+        array indexing, [0, 1]
+        first item = item[0]
+        second item = item[1]
+      */
+  const obj = {};
+  
+  input.forEach(item => obj[item[0]] = item[1])
+  
+  return obj;
 }
 
 function parseBody(str) {
   // Your code here
+  const first = firstStep(str);
+  const second = secondStep(first);
+  const third = thirdStep(second);
+  const fourth = fourthStep(third);
+  const fifth = fifthStep(fourth);
+
+  return fifth;
 }
 
 /******************************************************************************/
--- server.js
+++ server.js
@@ -6,6 +6,30 @@ let server;
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
 // Your code here
+const http = require('http');
+
+server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    let reqBody = '';
+
+    req.on('data', (data) => {
+        reqBody += data;
+    })
+
+    req.on('end', () => {
+        console.log(req.method, ' ', req.url);
+
+        req.body = parseBody(reqBody);
+
+        sendFormPage(req, res) //needs to be called even if the request doesnt have a body
+    });
+});
+
+const port = 5000;
+
+server.listen(port, () => console.log('Successfully started the server on port ', port));
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
